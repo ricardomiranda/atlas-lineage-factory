719ea9dbb909208d73a181f1b127409798bfddb2
Ricardo Miranda <mail@ricardomiranda.com>
2019-01-09 16:54:07 +0000

    new tests

--- tests/lineage/test_lineage.py
+++ tests/lineage/test_lineage.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import json
+import pathlib
 import pickle
 import unittest
 
@@ -7,17 +9,30 @@ from app.lineage.lineage import RequestNamedTuple
 from resources import pickled_objects
 
 
+def credentials():
+    auth_file = pathlib.Path(pathlib.Path(__file__).parent, "../../resources/auth.json")
+    with auth_file.open() as f:
+        auth = json.loads(f.read())
+
+    return (auth["user"], auth["password"])
+
+
 class TestLineage(unittest.TestCase):
 
     def test_payload_given_empty_arguments_should_return_valid_dict(self):
+        auth_file = pathlib.Path(pathlib.Path(__file__).parent, "../../resources/auth.json")
+        with auth_file.open() as f:
+            auth = f.read()
+        a = json.loads(auth)["auth"]
+
         actual = lineage.create_lineage_payload(
-                description="",
-                inputs=[{}],
-                name="",
-                outputs=[{}],
-                operation_type="",
-                qualified_name="",
-                type_name="")
+            description="",
+            inputs=[{}],
+            name="",
+            outputs=[{}],
+            operation_type="",
+            qualified_name="",
+            type_name="")
         expected = {"description": "",
                     "inputs": [{}],
                     "name": "",
@@ -30,19 +45,19 @@ class TestLineage(unittest.TestCase):
 
     def test_payload_given_arguments_with_valid_inputs_and_outputs_dicts_should_return_valid_dict(self):
         actual = lineage.create_lineage_payload(
-                description="",
-                inputs=[{
-                    "guid": "dc50d05c-2fbe-481b-ac6f-0180f1414fb5",
-                    "typeName": "hive_table"
-                }],
-                name="",
-                outputs=[{
-                    "guid": "dc50d05c-2fbe-481b-ac6f-0180f1414fb5",
-                    "typeName": "hive_table"
-                }],
-                operation_type="",
-                qualified_name="",
-                type_name="")
+            description="",
+            inputs=[{
+                "guid": "dc50d05c-2fbe-481b-ac6f-0180f1414fb5",
+                "typeName": "hive_table"
+            }],
+            name="",
+            outputs=[{
+                "guid": "dc50d05c-2fbe-481b-ac6f-0180f1414fb5",
+                "typeName": "hive_table"
+            }],
+            operation_type="",
+            qualified_name="",
+            type_name="")
         expected = {"description": "",
                     "inputs": [{
                         "guid": "dc50d05c-2fbe-481b-ac6f-0180f1414fb5",
@@ -60,13 +75,13 @@ class TestLineage(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_build_request_given_empty_arguments_should_return_empty_object(self):
-        actual = lineage.build_request_named_tuple(atlas_url="", credentials={})
+        actual = lineage.build_request_named_tuple(atlas_url="", credentials=("", ""))
         expected = RequestNamedTuple(method="GET",
-                                     headers={'Content-Type': 'application/json',
-                                              'Accept': 'application/json;charset=UTF-8'},
+                                     headers={"Content-Type": "application/json",
+                                              "Accept": "application/json;charset=UTF-8"},
                                      atlas_url="",
                                      payload={},
-                                     credentials={})
+                                     credentials=("", ""))
         self.assertEqual(expected, actual)
 
     def test_build_request_given_valid_arguments_should_return_valid_object(self):
@@ -87,7 +102,7 @@ class TestLineage(unittest.TestCase):
         atlas_url = "http://localhost:21000/api/atlas/types"
         credentials = {"user": "admin:admin"}
         actual = lineage.build_request_named_tuple(atlas_url=atlas_url,
-                                       payload=payload, credentials=credentials)
+                                                   payload=payload, credentials=credentials)
         expected = RequestNamedTuple(method="GET",
                                      headers={'Content-Type': 'application/json',
                                               'Accept': 'application/json;charset=UTF-8'},
@@ -97,26 +112,40 @@ class TestLineage(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_request_call_given_empty_request_named_tuple_should_return_request_object(self):
-        actual = pickle.dumps(lineage.prepare_request(
-                lineage.build_request_named_tuple(atlas_url="http://localhost:21000/api/atlas/entities?type=hive_table")))
+        actual = lineage.prepare_request(
+            lineage.build_request_named_tuple(
+                atlas_url="http://localhost:21000/api/atlas/entities?type=hive_table",
+                credentials=credentials()))
 
-        expected = 222
-        # expected = pickle.loads(pickled_objects.pickled_request_hive_tables)
+        expected = pickle.loads(pickled_objects.pickled_request_hive_tables)
 
-        self.assertEqual(expected, actual)
-#        self.assertEqual(
-#                [expected.method, expected.url, expected.headers, expected.body],
-#                [actual.method, actual.url, actual.headers, actual.body])
+        self.assertEqual(
+            [expected.method, expected.url, expected.headers, expected.body],
+            [actual.method, actual.url, actual.headers, actual.body])
 
-#    def test_create_lineage_prepare_empty_data(self):
-#        actual = pickle.dumps(lineage.create_lineage_prepare(
-#            atlas_url="http://localhost:21000/api/atlas/entities?type=hive_table", description="iiiiiiiiiiiii", inputs={"a":"a", "b":"b" }, operation_type="iiiii",
-#                name="iii", outputs={"a":"a", "b":"b"}, qualified_name="iiii", type_name="iiiiii"))
-        
-#        expected = 111
-        # expected = pickle.loads(pickled_objects.pickled_request_empty_data)
+    def test_create_lineage_prepare_empty_data(self):
+        actual = lineage.create_lineage_prepare(
+            atlas_url="http://localhost:21000/api/atlas", description="", inputs=[{}], operation_type="",
+            name="", outputs=[{}], qualified_name="", type_name="", credentials=credentials())
 
-#        self.assertEqual(expected, actual)
+        expected = pickle.loads(pickled_objects.pickled_request_empty_data)
+
+        self.assertEqual(
+            [expected.method, expected.url, expected.headers, expected.body],
+            [actual.method, actual.url, actual.headers, actual.body])
+
+    def test_create_lineage_prepare_simple_lineage(self):
+        actual = lineage.create_lineage_prepare(
+            atlas_url="http://localhost:21000/api/atlas",
+            description="Data flow form raw to canonical data model",
+            inputs=[{}], operation_type="",
+            name="", outputs=[{}], qualified_name="", type_name="")
+
+        expected = pickle.loads(pickled_objects.pickled_request_empty_data)
+
+        self.assertEqual(
+            [expected.method, expected.url, expected.headers, expected.body],
+            [actual.method, actual.url, actual.headers, actual.body])
 
 
 if __name__ == '__main__':
--- tests_integration/lineage/test_lineage.py
+++ tests_integration/lineage/test_lineage.py
@@ -1,20 +1,50 @@
 #!/usr/bin/python3
+import json
+import pathlib
 import unittest
 
 from app.lineage import lineage
 
+def credentials():
+    auth_file = pathlib.Path(pathlib.Path(__file__).parent, "../../resources/auth.json")
+    with auth_file.open() as f:
+        auth = json.loads(f.read())
+
+    return (auth["user"], auth["password"])
 
 class TestIntegrationLineage(unittest.TestCase):
 
-    def test_request_call_given_empty_request_named_tuple_should_return_request_object(self):
+    def test_request_call_given_no_credentials(self):
         actual = lineage.send_request(
             lineage.prepare_request(
-                lineage.build_request(
+                lineage.build_request_named_tuple(
                     atlas_url="http://localhost:21000/api/atlas/entities?type=hive_table"))).status_code
+
+        expected = 401
+
+        self.assertEqual(expected, actual)
+
+    def test_request_call_given_with_credentials(self):
+        actual = lineage.send_request(
+            lineage.prepare_request(
+                lineage.build_request_named_tuple(
+                    atlas_url="http://localhost:21000/api/atlas/entities?type=hive_table",
+                    credentials=credentials()))).status_code
 
         expected = 200
 
         self.assertEqual(expected, actual)
+
+    def test_request_call_given_empty_request_named_tuple2(self):
+        actual = lineage.send_request(
+            lineage.prepare_request(
+                lineage.build_request_named_tuple(
+                    atlas_url="http://localhost:21000/api/atlas/no_api"))).reason
+
+        expected = 200
+
+        self.assertEqual(expected, actual)
+
 
 if __name__ == '__main__':
     unittest.main()
